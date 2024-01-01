75a06512911450f60f97d20b5d4b9b02bd9276c6
sagarkarki903 <sagarkarki903@gmail.com>
2024-01-01 08:16:31 -0600

    added draw game functionality

--- app.js
+++ app.js
@@ -6,91 +6,107 @@ let msg = document.querySelector("#msg");
 
 resetBtn.classList.remove("hide-reset");
 
-let turnO= true; //if true then O, if false then X
+let turnO = true; // if true then O, if false then X
 
-//winning patterns
+// Winning patterns
 const winPatterns = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8], 
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8]
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8]
 ];
 
-const resetGame = ()=> {
-    turnO = true;
-    enableBoxes();
-    msgContainer.classList.add("hide");
-    resetBtn.classList.remove("hide-reset");
+const resetGame = () => {
+  turnO = true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+  resetBtn.classList.remove("hide-reset");
+  count = 0; // Resets count when starting a new game
+};
 
-    };
+const checkDraw = () => {
+  if (count === 9 && !checkForWinner()) {
+    noWinner(); // All boxes are filled, and no winner
+  }
+};
 
+const checkForWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+
+        showWinner(pos1Val); // End the game and show winner
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+let count = 0; // counts to check for draws after all 9 boxes are filled
 boxes.forEach((boxx) => {
-    boxx.addEventListener("click", ()=>{
-        console.log("box was clicked!");
-        
-        if(turnO){
-            boxx.innerText = "O";
-            turnO = false;
-        }
-        else{
-            boxx.innerText = "X";
-            turnO = true;
-        }
-        boxx.disabled = true;
+  boxx.addEventListener("click", () => {
+    console.log("box was clicked!");
 
-        // if(box.innerText === "O"){
-        //     box.innerText.
-
-        // }
-
-        checkWinner();
-    });
-}
-
-);
-const enableBoxes = () => { 
-    for(let box of boxes){
-        box.disabled =false;
-        box.innerText = "";
+    if (turnO) {
+      boxx.innerText = "O";
+      turnO = false;
+    } else {
+      boxx.innerText = "X";
+      turnO = true;
     }
+    boxx.disabled = true;
+
+    checkWinner();
+    count++; // Increment count after each move
+
+    if (count === 9) {
+      // Check for a draw after all boxes are filled
+      checkDraw();
+    }
+  });
+});
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
 };
 
-const disableBoxes = () => { 
-    for(let box of boxes){
-        box.disabled =true;
-    }
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
 };
 
-const showWinner = (winner)=> {
-    msg.innerText = `Congratulations, Winner is ${winner}`;
-    msgContainer.classList.remove("hide");
-    disableBoxes();
-    resetBtn.classList.add("hide-reset");
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+  resetBtn.classList.add("hide-reset");
+};
+
+const noWinner = () => {
+  msg.innerText = "It's a draw! Start a New Game.";
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+  resetBtn.classList.add("hide-reset");
 };
 
 const checkWinner = () => {
-    for(let pattern of winPatterns){
-            let pos1Val = boxes[pattern[0]].innerText;
-            let pos2Va1 =  boxes[pattern[1]].innerText;
-            let pos3Val =  boxes[pattern[2]].innerText;
+  if (!checkForWinner()) {
     
-
-    if(pos1Val != "" && pos2Va1 != ""  && pos3Val != ""){
-        if(pos1Val === pos2Va1 && pos2Va1 === pos3Val){
-            console.log("WINNER!", pos1Val);
-            showWinner(pos1Val);
-            break;
-
-        }
-    }
-}
-    
+  }
 };
-
 
 newGameBtn.addEventListener("click", resetGame);
 resetBtn.addEventListener("click", resetGame);
--- rough.js
+++ rough.js
@@ -1,18 +1,113 @@
-//arrow function practice
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
-function sum(a,  b)
-{
-return a+b;
+resetBtn.classList.remove("hide-reset");
 
-}
+let turnO = true; // if true then O, if false then X
 
-const addFun = (a, b) => {
-    return a+b;
-}
+// Winning patterns
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8]
+];
 
-console.log(addFun(3, 3));
+const resetGame = () => {
+  turnO = true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+  resetBtn.classList.remove("hide-reset");
+  count = 0; // Reset count when starting a new game
+};
 
-const myName = ()=> {
-    return"mero naam Sagar Karki ho.";
-}
-console.log(myName());
+const checkDraw = () => {
+  if (count === 9 && !checkForWinner()) {
+    noWinner(); // All boxes are filled, and no winner
+  }
+};
+
+const checkForWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log("WINNER!", pos1Val);
+        showWinner(pos1Val); // End the game and show winner
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+let count = 0; // Declare count outside the event listener
+boxes.forEach((boxx) => {
+  boxx.addEventListener("click", () => {
+    console.log("box was clicked!");
+
+    if (turnO) {
+      boxx.innerText = "O";
+      turnO = false;
+    } else {
+      boxx.innerText = "X";
+      turnO = true;
+    }
+    boxx.disabled = true;
+
+    checkWinner();
+    count++; // Increment count after each move
+
+    if (count === 9) {
+      // Check for a draw after all boxes are filled
+      checkDraw();
+    }
+  });
+});
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+  resetBtn.classList.add("hide-reset");
+};
+
+const noWinner = () => {
+  msg.innerText = "It's a draw! Start a New Game.";
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+  resetBtn.classList.add("hide-reset");
+};
+
+const checkWinner = () => {
+  if (!checkForWinner()) {
+    // Continue with other checks if there is no winner yet
+    // ...
+  }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
