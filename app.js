3c37b223eb494bcc8e598b79d6ff1e107baee433
sagarkarki903 <sagarkarki903@gmail.com>
2024-01-01 01:14:08 -0600

    jsRevision tictactoe format ready

---
+++ app.js
@@ -0,0 +1,32 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelectorAll("#reset");
+
+let turnO= true; //if true then O, if false then X
+
+//winning patterns
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8], 
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", ()=>{
+        console.log("box was clicked!");
+        if(turnO){
+            box.innerText = "O";
+            turnO = false;
+        }
+        else{
+            box.innerText = "X";
+            turnO = true;
+        }
+    });
+}
+
+);
---
+++ rough.js
@@ -0,0 +1,18 @@
+//arrow function practice
+
+function sum(a,  b)
+{
+return a+b;
+
+}
+
+const addFun = (a, b) => {
+    return a+b;
+}
+
+console.log(addFun(3, 3));
+
+const myName = ()=> {
+    return"mero naam Sagar Karki ho.";
+}
+console.log(myName());
